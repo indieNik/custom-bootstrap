a4c0e93881f6d79702e61e1628355aa0c0b7390f
Nikhil Patil <nikhil@nikhil.engagedly.in>
2018-11-27 17:13:48 +0530

    JS rendered SVG - DONE

--- js/draw_graph.js
+++ js/draw_graph.js
@@ -1,4 +1,8 @@
-function drawGraphLabel(svgId, position, value, inverted) {
+function appendToDom(svg, child) {
+    svg.appendChild(child);
+}
+
+function drawGraphLabel(svgId, position, value, inverted, state) {
     let svgns = "http://www.w3.org/2000/svg";
     let svg = document.getElementById(svgId);
     let label = document.createElementNS(svgns, "text");
@@ -7,11 +11,14 @@ function drawGraphLabel(svgId, position, value, inverted) {
     inverted ? renderedPosition = 'inverted-' + position : renderedPosition = position;
 
     let classVal = "graph__label graph__label--" + renderedPosition;
+    if(state) classVal += ' graph__label--' + state;
+
     // console.log('Final Class val: ', classVal);
     label.setAttributeNS(null, "class", classVal);
     if (value) label.textContent = value;
     
-    svg.appendChild(label);
+    // svg.appendChild(label);
+    appendToDom(svg, label);
 }
 
 function drawGraphLine(svgId, position, inverted, dashed) {
@@ -26,7 +33,8 @@ function drawGraphLine(svgId, position, inverted, dashed) {
     // console.log('Final Class val: ', classVal);
     graphLine.setAttributeNS(null, "class", classVal);
     
-    svg.appendChild(graphLine);
+    // svg.appendChild(graphLine);
+    appendToDom(svg, graphLine);
 }
 
 function drawGraphCircle(svgId, state) {
@@ -40,7 +48,8 @@ function drawGraphCircle(svgId, state) {
     // console.log('Final Class val: ', classVal);
     circle.setAttributeNS(null, "class", classVal);
     
-    svg.appendChild(circle);
+    // svg.appendChild(circle);
+    appendToDom(svg, circle);
 }
 
 function drawGraphMarker(svgId, inverted, state) {
@@ -61,29 +70,35 @@ function drawGraphMarker(svgId, inverted, state) {
         // console.log('Final Outer Class val: ', classVal);
         outerMarker.setAttributeNS(null, "class", classVal);
         svg.appendChild(outerMarker);
+        appendToDom(svg, outerMarker)
     }
     
-    svg.appendChild(marker);
+    // svg.appendChild(marker);
+    appendToDom(svg, marker);
 }
 
-function drawGraphConnectorLine(svgId, inverted, dashed) {
+function drawGraphConnectorLine(svgId, inverted, dashed, curved) {
     let svgns = "http://www.w3.org/2000/svg";
     let svg = document.getElementById(svgId);
     let graphConnectorLine = document.createElementNS(svgns, "path");
     let classVal = "connector__line";
-    if (dashed) classVal += " connector__line--dashed"
-    if (inverted) classVal += " connector__line--inverted"
+    if (dashed) classVal += " connector__line--dashed";
+    if (inverted) classVal += " connector__line--inverted";
+    if (curved) classVal += " connector__line--curve";
 
     // console.log('Final Class val: ', classVal);
     graphConnectorLine.setAttributeNS(null, "class", classVal);
     
-    svg.appendChild(graphConnectorLine);
+    // svg.appendChild(graphConnectorLine);
+    appendToDom(svg, graphConnectorLine);
 }
 
-setTimeout(async function(){
+setTimeout(() => {
 
-    let json = {
-        'node1': {
+    let jsonData = [
+        {    
+            'id': 'node1',
+            'type': 'node',
             'topLabel': 'Jan 1',
             'middleLabel': '500',
             'bottomLabel': 'Assigned',
@@ -93,7 +108,9 @@ setTimeout(async function(){
             'leftDashed' : false,
             'rightDashed' : false
         },
-        'node2': {
+        {    
+            'id': 'node2',
+            'type': 'node',
             'topLabel': 'Jan 10',
             'middleLabel': '',
             'bottomLabel': 'Shared',
@@ -103,7 +120,9 @@ setTimeout(async function(){
             'leftDashed' : false,
             'rightDashed' : false
         },
-        'node3': {
+        {    
+            'id': 'node3',
+            'type': 'node',
             'topLabel': 'Jan 18',
             'middleLabel': '',
             'bottomLabel': 'Assigned',
@@ -111,19 +130,29 @@ setTimeout(async function(){
             'status': 'shared',
             'activeState' : 'normal',
             'leftDashed' : false,
-            'rightDashed' : true
+            'rightDashed' : false
         },
-        'node4': {
+        {    
+            'id': 'graph_connector_curve',
+            'type': 'connector',
+            'inverted': false,
+            'dashed' : false
+        },
+        {    
+            'id': 'node4',
+            'type': 'node',
             'topLabel': 'Employee',
             'middleLabel': '',
             'bottomLabel': 'Feb 2',
             'inverted': true,
             'status': 'shared',
             'activeState' : 'normal',
-            'leftDashed' : true,
+            'leftDashed' : false,
             'rightDashed' : false
         },
-        'node5': {
+        {    
+            'id': 'node5',
+            'type': 'node',
             'topLabel': 'Manager',
             'middleLabel': '',
             'bottomLabel': 'Feb 23',
@@ -133,7 +162,9 @@ setTimeout(async function(){
             'leftDashed' : false,
             'rightDashed' : false
         },
-        'node6': {
+        {    
+            'id': 'node6',
+            'type': 'node',
             'topLabel': 'Additional',
             'middleLabel': '',
             'bottomLabel': 'Mar 1',
@@ -143,47 +174,52 @@ setTimeout(async function(){
             'leftDashed' : false,
             'rightDashed' : false
         }
-    };
+    ];
+    let graph_container = document.createElement('div');
+    graph_container.setAttribute('id', 'graph_container_new');
+    graph_container.setAttribute('class', 'graph-container');
+    
+    document.body.appendChild(graph_container);
+    
+    jsonData.forEach( (node, index) => {
+        let svgns = "http://www.w3.org/2000/svg";
+        if(node.type === 'node') {
+            // Create a SVG DOM element to draw each graph node
+            let graph = document.createElementNS(svgns, 'svg');
+            graph.setAttribute('id', node.id);
+            graph.setAttribute('class', 'graph');
+            graph_container.appendChild(graph);
+    
+            drawGraphCircle(node.id, node.status);
+            drawGraphLabel(node.id, 'top', node.topLabel, node.inverted, 'normal');
+            drawGraphLabel(node.id, 'middle', node.middleLabel, node.inverted, node.activeState);
+            drawGraphLabel(node.id, 'bottom', node.bottomLabel, node.inverted, node.activeState);
+            drawGraphLine(node.id, 'left', node.inverted, node.leftDashed);
+            drawGraphLine(node.id, 'right', node.inverted, node.rightDashed);
+            drawGraphMarker(node.id, node.inverted, node.activeState);  
 
-    let graph_container = document.getElementById('graph-container');
-
-    for (const nodeId in json) {
-
-        const node = json[nodeId];
-        
-        // Create a SVG DOM element to paint
-        let graph = document.createElement('svg');
-        graph.setAttribute('id', nodeId);
-        graph.setAttribute('class', 'graph');
-        graph_container.appendChild(graph);
-
-        drawGraphCircle(nodeId, node['status']);
-        drawGraphLabel(nodeId, 'top', node['topLabel'], node['inverted']);
-        drawGraphLabel(nodeId, 'middle', node['middleLabel'], node['inverted']);
-        drawGraphLabel(nodeId, 'bottom', node['bottomLabel'], node['inverted']);
-        drawGraphLine(nodeId, 'left', node['inverted'], node['leftDashed']);
-        drawGraphLine(nodeId, 'right', node['inverted'], node['rightDashed']);
-        drawGraphMarker(nodeId, node['inverted'], node['activeState']);
-        await sleep(500);
-
-        if(nodeId != 'node8') {
-            let graph_container = document.getElementById('graph-container');
-            let graph_connector = document.createElement('svg');
-            let graph_connector_id = 'connector' + parseInt(Object.keys(json).indexOf(nodeId) + 1);
-            graph_connector.setAttribute('id', graph_connector_id);
-            graph_connector.setAttribute('class', 'connector');
-            graph_container.appendChild(graph_connector);
+            if (jsonData.length > index - 1) {
+                let graph_connector = document.createElementNS(svgns, 'svg');
+                let graph_connector_id = 'connector' + (index+1);
+                graph_connector.setAttribute('id', graph_connector_id);
+                graph_connector.setAttribute('class', 'connector');
+                graph_container.appendChild(graph_connector);
+                // Draw a dashed connector line only if the current node's right graphLine is dashed
+                drawGraphConnectorLine(graph_connector_id, node.inverted, node.rightDashed, false);
+            }
+        } else {
+            // Remove the previous connector
+            let previousConnector = document.querySelectorAll(".connector:last-child");
+            console.log('Removing: ', previousConnector);
+            previousConnector[0].remove();
+            // Draw a curved connector
+            let graph_connector_curve = document.createElementNS(svgns, 'svg');
+            let graph_connector_curve_id = node.id;
+            graph_connector_curve.setAttribute('id', graph_connector_curve_id);
+            graph_connector_curve.setAttribute('class', 'connector connector--curve');
+            graph_container.appendChild(graph_connector_curve);
             // Draw a dashed connector line only if the current node's right graphLine is dashed
-            drawGraphConnectorLine(graph_connector_id, node['inverted'], node['rightDashed']);
-            await sleep(1000);
+            drawGraphConnectorLine(graph_connector_curve_id, node.inverted, node.dashed, true);
         }
-    }
-
-    console.log(graph_container);
-
-}, 0);
-
-function sleep(ms) {
-    console.log('Sleeping for ' + ms + ' seconds..');
-    return new Promise(resolve => setTimeout(resolve, ms));
-}  
+    });
+}, 1000);
